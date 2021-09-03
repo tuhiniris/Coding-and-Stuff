aa2f26310a54d5c14d81de87bf748f978125d9d9
Tuhinadri Banerjee <tuhiniris@gmail.com>
2021-09-03 18:58:16 +0530

    updatte

---
+++ TestA.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class TestA {	
+	public static void main (String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		System.out.println(n+x);
+	}
+}
+
